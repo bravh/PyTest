10634cd69bee828ed36867a414736592e3e20922
boris <bravh1@gmail.com>
2022-04-30 15:35:55 +0300

    make it better

--- website/static/checklist.js
+++ website/static/checklist.js
@@ -1,26 +1,47 @@
 
 let page = 1;
+const cabinet = document.querySelector('.cabinet');
+const special = document.querySelector('.special');
 buttons = document.querySelector('.buttons');
 nextbutton = document.querySelector('.btn-next');
 function next(){
     const current = document.querySelector('._'+ page);
     const next = document.querySelector('._'+ (page + 1));
-    if(next){
-    console.log(current);
+    if(next && page <= 10){
+        if(special.classList.contains("checked")){
+    /*console.log(current);*/
     current.classList.remove("on");
     current.classList.add("off");
     next.classList.remove('off');
     next.classList.add('on');
     page = page + 1;
-    }
-    else{ page = page -1;};
-    nextbutton.style= "display: none";
+    };
+   /* nextbutton.style= "display: none";*/
+   console.log(page);
+    };
 
+    if(next && page <= 10 || next && page >= 11){
+        if(cabinet.classList.contains("checked")){
+           
+    /*console.log(current);*/
+    current.classList.remove("on");
+    current.classList.add("off");
+    if(page<11){page=11;};
+    const nextfar = document.querySelector('._'+ (page + 1));
+    nextfar.classList.remove('off');
+    nextfar.classList.add('on');
+    page = page + 1;
+    };
+   /* nextbutton.style= "display: none";*/
+   console.log(page);
+    };
+    
 };
 function prev(){
-    const current = document.querySelector('._'+ page);
-    const prev = document.querySelector('._'+ (page - 1));
-    if(prev){
+    const current = document.querySelector('._' + page);
+    const prev = document.querySelector('._' + (page - 1));
+    if(prev && !prev.classList.contains("far")){
+        
     console.log(current);
     current.classList.remove("on");
     current.classList.add("off");
@@ -28,13 +49,20 @@ function prev(){
     prev.classList.add('on');
     page = page - 1 ;
     };
+    if(prev && prev.classList.contains("far")){
+        current.classList.remove("on");
+        current.classList.add("off");
+        page = 1;
+        const nextfar = document.querySelector('._'+ (page));
+        nextfar.classList.remove('off');
+        nextfar.classList.add('on');
+        
+        };
     nextbutton.style= "display: all";
+    console.log(page);
 
 
 };
-cabinet = document.querySelector('.cabinet');
-special = document.querySelector('.special');
-
 
 
 cabinet.addEventListener("click" , (e)=>{ 
